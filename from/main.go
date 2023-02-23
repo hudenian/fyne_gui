4cccd5e6f954a3a23669a16b8b728288a3adeef0
hudenian <hudenian@163.com>
2023-02-23 18:30:40 +0800

    adjust layout

--- from/main.go
+++ from/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -21,37 +22,36 @@ var (
 
 func main() {
 	myApp := app.New()
-	w := myApp.NewWindow("UserErc721A合约查询")
+	w := myApp.NewWindow("Erc721A contract query")
 
-	rpc.Set("https://eth-goerli.public.blastapi.io")
-	contractAddr.Set("0x8ece72a85879e2c289b32f38ea35c307b8e50d1c")
+	_ = rpc.Set("https://eth-goerli.public.blastapi.io")
+	_ = contractAddr.Set("0x8ece72a85879e2c289b32f38ea35c307b8e50d1c")
 
 	rpcEntry, contractAddressEntry := conditionFrom()
 
 	from := widget.NewForm(
-		widget.NewFormItem("rpc:", rpcEntry),
-		widget.NewFormItem("contract address:", contractAddressEntry),
+		widget.NewFormItem("rpc", rpcEntry),
+		widget.NewFormItem("contract address", contractAddressEntry),
 	)
-
-	tokenIdEntry, ownerLabel, queryBtn := queryOwnerOf()
-	from.Append("tokenId", tokenIdEntry)
-	from.Append("owner", ownerLabel)
-	from.Append("query owner", queryBtn)
+	//生成查询ownerOf接口表单
+	queryOwnerOf(from)
 
 	c := container.NewVBox(from)
 	w.SetContent(c)
-	w.Resize(fyne.NewSize(640, 460))
+	w.Resize(fyne.NewSize(720, 460))
 	w.ShowAndRun()
 }
 
 /**
 查询token的owner表单
 */
-func queryOwnerOf() (*widget.Entry, *widget.Label, *widget.Button) {
+func queryOwnerOf(from *widget.Form) {
 	tokenId := widget.NewEntry()
 	tokenId.SetText("0")
-	owner := widget.NewLabel("")
-	ownerQBtn := widget.NewButton("query owner", func() {
+	totalSupply := widget.NewEntry()
+	owner := widget.NewEntry()
+	baseUri := widget.NewEntry()
+	ownerQBtn := widget.NewButton("query", func() {
 		rpcUrl, _ := rpc.Get()
 		client, err := ethclient.Dial(rpcUrl)
 		if err != nil {
@@ -59,20 +59,38 @@ func queryOwnerOf() (*widget.Entry, *widget.Label, *widget.Button) {
 			return
 		}
 		defer client.Close()
-		owner.SetText("success to Dial")
+		totalSupply.SetText("success to Dial")
 		contractAddress, _ := contractAddr.Get()
 		address := common.HexToAddress(contractAddress)
 		instance, err := contract.NewUserERC721A(address, client)
 		if err != nil {
 			owner.SetText(err.Error())
 		}
+		supply, _ := instance.TotalSupply(&bind.CallOpts{Pending: true})
+		totalSupply.SetText(supply.String())
+
 		valInt, _ := strconv.Atoi(tokenId.Text)
-		address, _ = instance.OwnerOf(&bind.CallOpts{Pending: true}, big.NewInt(int64(valInt)))
-		owner.SetText(address.String())
+		addr, _ := instance.OwnerOf(&bind.CallOpts{Pending: true}, big.NewInt(int64(valInt)))
+		if addr.String() == "0x0000000000000000000000000000000000000000" {
+			owner.SetText("token id not exist")
+		} else {
+			owner.SetText(addr.String())
+		}
+
+		uri, _ := instance.TokenURI(&bind.CallOpts{Pending: true}, big.NewInt(int64(valInt)))
+		if len(uri) > 10 {
+			baseUri.SetText(fmt.Sprintf("%s%s", "https://gateway.ipfs.io/ipfs/", uri[7:]))
+		} else {
+			baseUri.SetText("token id not exist")
+		}
 	})
 	ownerQBtn.Importance = widget.HighImportance
 
-	return tokenId, owner, ownerQBtn
+	from.Append("totalSupply", totalSupply)
+	from.Append("tokenId", tokenId)
+	from.Append("owner", owner)
+	from.Append("token uri", baseUri)
+	from.Append("", ownerQBtn)
 }
 
 /**
